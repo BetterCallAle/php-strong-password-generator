5c3babdeb3a114992940214a54f5cdd6721e2fa6
BetterCallAle <dap22091998@gmail.com>
2022-11-28 15:21:15 +0100

    project scaffolding

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+</head>
+<body>
+    <form action="index.php" method:"GET">
+        <label for="length">Scegli la lunghezza della tua Password</label>
+        <input type="number" name:"length" id="length">
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
